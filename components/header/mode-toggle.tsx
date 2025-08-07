0920b82b0996ed1225ebccfdb0fe2d0972ecac07
Muhammad Zaki As Shidiqi <zakiawuawupurworejo16@gmail.com>
2025-08-07 21:19:46 +0700

    chore(mode-toggle): remove unused Button import

--- components/header/mode-toggle.tsx
+++ components/header/mode-toggle.tsx
@@ -4,8 +4,6 @@ import * as React from "react";
 import { Moon, Sun, Monitor } from "lucide-react";
 import { useTheme } from "next-themes";
 
-import { Button } from "@/components/ui/button";
-
 const ModeToggle = () => {
   const { theme, setTheme } = useTheme();
 
