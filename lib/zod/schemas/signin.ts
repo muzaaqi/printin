ebde6752a4a53ba2fc7e76fc6b17354f2bf4f5bf
Muhammad Zaki As Shidiqi <zakiawuawupurworejo16@gmail.com>
2025-08-08 00:25:08 +0700

    refactor(signin): move signin validation to zod schema

--- lib/zod/schemas/signin.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const signInSchema = z.object({
-  email: z.string().email("Email tidak valid"),
-  password: z.string().min(6, "Password harus memiliki setidaknya 6 karakter"),
-});
-
-export type SignInInput = z.infer<typeof signInSchema>;
