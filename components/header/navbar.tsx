6cd966d2b7d575e7b88c57c31d2132aa25ac54bc
Muhammad Zaki As Shidiqi <zakiawuawupurworejo16@gmail.com>
2025-08-07 15:01:17 +0700

    refactor(header): move navbar, profile, and mode-toggle components into header directory

--- components/header/navbar.tsx
+++ components/header/navbar.tsx
@@ -1,12 +1,15 @@
 import Link from "next/link";
 import React from "react";
-import Profile from "./profile";
+import Profile from "@/components/header/profile";
 
 const Navbar = () => {
   return (
     <div className="w-full sticky bg-background/10 top-0 backdrop-blur-md z-50 border-b border-accent-foreground/20">
       <div className="mx-auto flex justify-between items-center p-2">
-        <Link href="/" className="flex items-center text-2xl font-bold ml-2 sm:mr-9">
+        <Link
+          href="/"
+          className="flex items-center text-2xl font-bold ml-2 sm:mr-9"
+        >
           <span className="text-zinc-400">NGE</span>
           <span className="text-popover-foreground">PRINT</span>
         </Link>
--- components/header/profile.tsx
+++ components/header/profile.tsx
@@ -16,7 +16,7 @@ const Profile = () => {
         <DropdownMenuTrigger>
           <div className="flex items-center space-x-2 px-3 py-2 rounded-lg hover:bg-foreground/10 transition-all duration-300">
             <span className="text-lg font-semibold">Nama Kamu</span>
-            <div className="border-3 p-0.5 rounded-full hover:border-foreground transition-all duration-200">
+            <div className="border-3 p-0.5 rounded-full hover:border-foreground transition-all duration-00">
               <div className="bg-foreground w-[40] h-[40] rounded-full"></div>
             </div>
           </div>
