4f8e70b7059983535d52c015457c85848bdc8b39
Muhammad Zaki As Shidiqi <zakiawuawupurworejo16@gmail.com>
2025-08-16 01:10:06 +0700

    feat(dashboard): add new services and transactions pages

---
+++ app/dashboard/transactions/page.tsx
@@ -0,0 +1,12 @@
+import TransactionsTable from '@/components/dashboard/transactions/transactions-table'
+import React from 'react'
+
+const TransactionsPage = () => {
+  return (
+    <div>
+      <TransactionsTable />
+    </div>
+  )
+}
+
+export default TransactionsPage
