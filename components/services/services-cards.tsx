f6065f2c45096c7b8815685d029d25dd8e75800b
Muhammad Zaki As Shidiqi <zakiawuawupurworejo16@gmail.com>
2025-08-20 22:04:15 +0700

    refactor(services): replace old ServicesCards with new implementation

--- components/services/services-cards.tsx
+++ components/services/services-cards.tsx
@@ -1,13 +1,15 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from "react";
 import ServicesCard from "./services-card";
-import { GetAllServicesStokRealtime, Services } from "@/features/get-all-services-realtime";
+import {
+  GetAllServicesStokRealtime,
+  Services,
+} from "@/features/get-all-services-realtime";
 import { Skeleton } from "../ui/skeleton";
 
-const ServicesCards = ({ isAuthenticated } : {isAuthenticated: boolean;}) => {
+const ServicesCards = ({ isAuthenticated }: { isAuthenticated: boolean }) => {
   const [services, setServices] = useState<Services[]>([]);
   const [loading, setLoading] = useState(true);
-  
 
   useEffect(() => {
     let unsubscribe: (() => void) | undefined;
@@ -28,8 +30,11 @@ const ServicesCards = ({ isAuthenticated } : {isAuthenticated: boolean;}) => {
     <div className="mx-auto flex flex-col justify-center px-4 pb-10 sm:px-6 md:grid md:grid-cols-2 lg:grid-cols-3 lg:px-8">
       {loading
         ? Array.from({ length: 10 }).map((_, i) => (
-            <div className="mx-auto w-full py-10 sm:px-6 lg:px-8" key={i}>
-              <Skeleton className="w-120 md:w-90 h-70 md:h-130 mx-auto" />
+            <div
+              className="mx-auto w-full py-10 sm:px-6 md:gap-5 lg:px-8"
+              key={i}
+            >
+              <Skeleton className="mx-auto h-70 w-120 md:h-130 md:w-80 lg:w-90" />
             </div>
           ))
         : services.map((service) => (
