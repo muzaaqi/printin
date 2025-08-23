a907b655de51c49b5492433083f07b3c29b9f401
Muhammad Zaki As Shidiqi <zakiawuawupurworejo16@gmail.com>
2025-08-23 23:23:38 +0700

    refactor(ui): improve courier register form and related pages with validation & prefill

--- app/(user)/courier/register/page.tsx
+++ app/(user)/courier/register/page.tsx
@@ -4,11 +4,9 @@ import React from "react";
 
 const CourierRegisterPage = async () => {
   const user = await getCurrentUser();
-
-  
   return (
     <div className="mx-auto flex h-screen items-center justify-center">
-      <RegisterForm />
+      <RegisterForm user={user} />
     </div>
   );
 };
--- components/courier/register-form.tsx
+++ components/courier/register-form.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 import {
   Card,
@@ -13,14 +13,18 @@ import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 import { Button } from "../ui/button";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { courierRegisterSchema, CourierRegisterSchema } from "@/lib/schema/courier-register";
+import { User } from "@supabase/supabase-js";
 
-const RegisterForm = () => {
-  const form = useForm();
+const RegisterForm = ({user} : {user: User}) => {
+  const form = useForm<CourierRegisterSchema>({
+    resolver: zodResolver(courierRegisterSchema),
+  });
 
-  
   return (
-    <Card className="min-w-sm max-w-md mx-auto">
-      <form>
+    <Card className="mx-auto max-w-md min-w-sm">
+      <form onSubmit={form.handleSubmit((data) => console.log(data))}>
         <CardHeader>
           <CardTitle>Courier Registration</CardTitle>
           <CardDescription>
@@ -30,15 +34,54 @@ const RegisterForm = () => {
         <CardContent className="space-y-4 py-6">
           <div className="space-y-3">
             <Label htmlFor="name">Name</Label>
-            <Input id="name" placeholder="Enter your name" />
+            <Input
+              {...form.register("name")}
+              defaultValue={user?.user_metadata.full_name}
+              type="text"
+              id="name"
+              placeholder="Enter your name"
+            />
+            {form.formState.errors.name && (
+              <span className="text-destructive">{form.formState.errors.name.message}</span>
+            )}
           </div>
           <div className="space-y-3">
             <Label htmlFor="email">Email</Label>
-            <Input id="email" placeholder="Enter your email" />
+            <Input
+              {...form.register("email")}
+              defaultValue={user?.email}
+              type="email"
+              id="email"
+              placeholder="Enter your email"
+            />
+            {form.formState.errors.email && (
+              <span className="text-destructive">{form.formState.errors.email.message}</span>
+            )}
           </div>
           <div className="space-y-3">
             <Label htmlFor="phone">Phone</Label>
-            <Input id="phone" placeholder="Enter your phone number" />
+            <Input
+              {...form.register("phone")}
+              defaultValue={user?.phone}
+              type="tel"
+              id="phone"
+              placeholder="Enter your phone number"
+            />
+            {form.formState.errors.phone && (
+              <span className="text-destructive">{form.formState.errors.phone.message}</span>
+            )}
+          </div>
+          <div className="space-y-3">
+            <Label htmlFor="area">Area</Label>
+            <Input
+              {...form.register("area")}
+              type="text"
+              id="area"
+              placeholder="Enter your area"
+            />
+            {form.formState.errors.area && (
+              <span className="text-destructive">{form.formState.errors.area.message}</span>
+            )}
           </div>
         </CardContent>
         <CardFooter>
